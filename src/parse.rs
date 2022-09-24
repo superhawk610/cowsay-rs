a30646a0d162ba6a1df298ec8bc7feb64f3178c0
Aaron Ross <superhawk610@gmail.com>
2022-09-23 23:23:39 -0700

    restructure parser
    Parsing in two passes simplifies the inner tokenizer (it doesn't have to
    worry about avoiding the terminating EOC if its already been removed in
    a separate pass).

--- src/parse.rs
+++ src/parse.rs
@@ -11,7 +11,10 @@ pub enum Token {
 }
 
 pub fn parse(filename: &str, template: &str) -> Result<Vec<Token>, ()> {
-    return match tokenizer().parse(template) {
+    return match template_extractor()
+        .parse(template)
+        .and_then(|t| tokenizer().parse(t))
+    {
         Ok(tokens) => Ok(tokens),
         Err(errors) => {
             let mut report =
@@ -30,34 +33,7 @@ pub fn parse(filename: &str, template: &str) -> Result<Vec<Token>, ()> {
 }
 
 fn tokenizer() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
-    let space_or_tab = filter(|c: &char| *c == ' ' || *c == '\t');
-
-    let prelude = just("$the_cow")
-        .padded()
-        .then(just('=').padded().or_not())
-        .then(just("<<").padded())
-        .then(
-            just('"')
-                .or_not()
-                .then(just("EOC"))
-                .then(just('"').or_not())
-                .then(space_or_tab.or_not()),
-        )
-        .then(just(";").or_not())
-        .then(text::newline());
-
-    let comment = just('#')
-        .then(
-            take_until(text::newline())
-                .map(|(i, _)| i)
-                .collect::<String>()
-                .map(Token::Comment),
-        )
-        .map(|(_, comment)| comment);
-
-    let ignored_line = choice((text::newline(), comment.ignored()));
-
-    let cow_part = choice((
+    choice((
         just("$thoughts").to(Token::Thoughts),
         just("${thoughts}").to(Token::Thoughts),
         just("$tongue").to(Token::Tongue),
@@ -74,33 +50,52 @@ fn tokenizer() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
         just("\\$").to(Token::Text("$".to_string())),
         just('\\').to(Token::Text("\\".to_string())),
         just('$').to(Token::Text("$".to_string())),
-        // TODO: there's gotta be a simpler way to do this
-        just('E')
-            .then_ignore(none_of("O").rewind())
-            .to(Token::Text("E".to_string())),
-        just("EO")
-            .then_ignore(none_of("C").rewind())
-            .to(Token::Text("EO".to_string())),
-        just("EOC")
-            .then_ignore(none_of("\n").rewind())
-            .to(Token::Text("EOC".to_string())),
         // if no keywords match, continue iterating until encountering a character
         // that _might_ start a keyword, or end the input; this means that the output
-        // may contain runs of `Token::Text` delimited by `$` or `E`, but that's fine
-        none_of("$E\\")
+        // may contain runs of `Token::Text` delimited by `$` or `\`, but that's fine
+        none_of("$\\")
             .repeated()
             .at_least(1)
             .collect::<String>()
             .map(Token::Text),
-    ));
+    ))
+    .repeated()
+    .then_ignore(end())
+}
+
+fn template_extractor() -> impl Parser<char, String, Error = Simple<char>> {
+    let prelude = just("$the_cow")
+        .padded()
+        .then(just('=').padded().or_not())
+        .then(just("<<").padded())
+        .then(
+            just('"')
+                .or_not()
+                .then(just("EOC"))
+                .then(just('"').or_not())
+                .then(one_of(" \t").or_not()),
+        )
+        .then(just(";").or_not())
+        .then(text::newline());
+
+    let comment = just('#')
+        .then(
+            take_until(text::newline())
+                .map(|(i, _)| i)
+                .collect::<String>()
+                .map(Token::Comment),
+        )
+        .map(|(_, comment)| comment);
+
+    let ignored_line = choice((text::newline(), comment.ignored()));
 
     empty()
         .then(ignored_line.repeated())
         .then(prelude)
         .ignore_then(
-            cow_part
-                .repeated()
-                .then_ignore(just("EOC").then(text::newline())),
+            take_until(just("EOC").then(text::newline()))
+                .map(|(i, _)| i)
+                .collect::<String>(),
         )
         .then_ignore(ignored_line.repeated())
         .then_ignore(end())
